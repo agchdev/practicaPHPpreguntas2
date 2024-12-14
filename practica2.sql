1ff0e9dba4a329957ce096ef4b1c500c2cb8dbe7
agchdev <alex.chinchi02@gmail.com>
2024-12-14 19:39:59 +0100

    cambios

--- practica2.sql
+++ practica2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-12-2024 a las 23:15:22
+-- Tiempo de generación: 14-12-2024 a las 19:39:19
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -60,7 +60,7 @@ CREATE TABLE `usuarios` (
   `usuario` varchar(50) NOT NULL,
   `tmpInicio` timestamp NULL DEFAULT current_timestamp(),
   `tmpFinal` timestamp NULL DEFAULT NULL,
-  `tmpTotal` timestamp NULL DEFAULT NULL,
+  `tmpTotal` int(3) DEFAULT NULL,
   `arrayPreg` varchar(20) NOT NULL,
   `arrayAciertos` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
@@ -70,26 +70,8 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`usuario`, `tmpInicio`, `tmpFinal`, `tmpTotal`, `arrayPreg`, `arrayAciertos`) VALUES
-('', '2024-12-12 16:38:53', NULL, NULL, ',9,2,10,6,1', NULL),
-('agch', '2024-12-12 16:30:32', NULL, NULL, ',4,8,6,1,9', NULL),
-('agch.obj', '2024-12-12 16:31:35', NULL, NULL, ',5,4,3,9,8', NULL),
-('agch.obj2', '2024-12-12 16:36:14', NULL, NULL, ',7,9,4,6,2', NULL),
-('agch12', '2024-12-12 16:33:54', NULL, NULL, ',6,3,1,8,10', NULL),
-('agch222222', '2024-12-12 16:43:53', NULL, NULL, ',5,10,7,2,1', NULL),
-('agchobj on yt', '2024-12-12 16:33:16', NULL, NULL, ',9,10,1,4,5', NULL),
-('agchobj22', '2024-12-12 16:37:44', NULL, NULL, ',6,3,4,10,2', NULL),
-('asdfawdf', '2024-12-12 16:40:16', NULL, NULL, ',5,7,4,10,2', NULL),
-('dw', '2024-12-12 16:34:29', NULL, NULL, ',9,7,4,3,5', NULL),
-('dwdw', '2024-12-12 16:34:42', NULL, NULL, ',6,10,5,2,1', NULL),
-('dwdwdwdwdasdfasdf', '2024-12-12 16:45:00', NULL, NULL, ',9,5,6,7,1', NULL),
-('jijija', '2024-12-12 17:07:27', NULL, NULL, ',10,3,5,8,6', NULL),
-('jijija2', '2024-12-12 17:08:39', NULL, NULL, ',9,6,8,2,1', NULL),
-('perroflauta', '2024-12-12 16:45:33', NULL, NULL, ',4,7,9,2,6', NULL),
-('perry', '2024-12-12 19:43:17', NULL, NULL, ',5,6,9,2,3', NULL),
-('perry2', '2024-12-12 19:44:10', NULL, NULL, ',8,4,3,2,9', NULL),
-('perry3', '2024-12-12 19:48:06', NULL, NULL, ',8,5,6,3,2', NULL),
-('wdwd', '2024-12-12 16:38:04', NULL, NULL, ',5,4,3,10,7', NULL),
-('wewe', '2024-12-12 16:39:47', NULL, NULL, ',7,3,10,4,8', NULL);
+('agch', '2024-12-14 17:35:02', '2024-12-14 18:13:40', 2318, '', NULL),
+('andres', '2024-12-14 18:14:06', '2024-12-14 18:37:58', 1432, '', NULL);
 
 --
 -- Índices para tablas volcadas
